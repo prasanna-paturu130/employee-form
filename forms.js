f6516a86873891d13a353aeac15d52a00997b45c
prasanna-paturu130 <prasanna.paturu130@gmail.com>
2022-06-07 22:21:35 +0530

    updated

--- forms.js
+++ forms.js
@@ -1,1 +1,20 @@
-console.log("Welcome for forms js file");
+$(document).ready(function(){
+    var errors = $("#errors");
+    errors.hide();
+    var form = $('#employeForm');
+    var errorMessae = '';
+    form.on('submit',function() {
+        errors.hide();
+        if($('#Lname').val().length > 8) {
+            errorMessae = 'Should not be more than eight characters';
+            errors.val(errorMessae).show();
+            return false;
+        }
+        if(parseInt($('#age').val()) > 35){
+            errorMessae = 'Age Should not be more than 35';
+            errors.val(errorMessae).show();
+            return false;
+        }
+        alert('ready to submit');
+    });
+});
